44fb7eede7e9f4ad7e57f450762ce3b6cae18e5b
Venkata Shanmukha Korada <venkatashanmukhak.in@mouritech.com>
2020-06-12 14:04:42 +0530

    Initial Commit

---
+++ public/js/app.js
@@ -0,0 +1,24 @@
+console.log('client side js file is loaded!')
+const weatherForm = document.querySelector('form')
+const search = document.querySelector('input')
+const message1 = document.querySelector('#msg1')
+message1.textContent = 'From Javascript'
+const message2 = document.querySelector('#msg2')
+message2.textContent = 'From JS'
+weatherForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+    const location = search.value
+    console.log(location)
+    message1.textContent = 'Loading...'
+    message2.textContent = ''
+    fetch('http://localhost:3000/weather?address=' + location).then((response) => {
+        response.json().then((data) => {
+            if (data.error) {
+                message1.textContent = data.error
+            } else {
+                message1.textContent = data.location
+                message2.textContent = data.forecast
+            }
+        })
+    })
+})
---
+++ src/utils/forecast.js
@@ -0,0 +1,17 @@
+const request = require('request')
+const forecast = (latitude, longitude, callback) => {
+    const url = 'http://api.weatherstack.com/current?access_key=066c559a7996e01dab7ab5a61cbc4b13&query= ' + latitude + ',' + longitude + '&units=m'
+
+    request({url , json: true} , (error, {body}) => {
+        if ( error) {
+                callback('unable to connect to weather services', undefined)
+        } else if(body.error) {
+            callback('unable to find location' , undefined)
+        } else {
+            callback(undefined, 
+                console.log(body.current.weather_descriptions[0] + '.Its currently ' + body.current.temperature + ' out but it feels like ' + body.current.feelslike + ' degrees out')
+            )
+        }
+    })
+}
+module.exports = forecast
