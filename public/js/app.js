b81bc57f09af4362c33cc00097ce39df72dcc0c1
Venkata Shanmukha Korada <venkatashanmukhak.in@mouritech.com>
2020-06-12 17:38:18 +0530

    set up app for heroku

--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,6 @@
 
 const weatherForm = document.querySelector('form')
-const search = document.querySelector('i nput')
+const search = document.querySelector('input')
 const message1 = document.querySelector('#msg1')
 message1.textContent = 'From Javascript'
 const message2 = document.querySelector('#msg2')
@@ -11,7 +11,7 @@ weatherForm.addEventListener('submit', (e) => {
     console.log(location)
     message1.textContent = 'Loading...'
     message2.textContent = ''
-    fetch('http://localhost:3000/weather?address=' + location).then((response) => {
+    fetch('/weather?address=' + location).then((response) => {
         response.json().then((data) => {
             if (data.error) {
                 message1.textContent = data.error
